ffc4f7ab938024fb3113509b0aa949fad7af35bd
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2021-12-26 00:50:02 +0100

    first commit

---
+++ 1 - Danny's Diner/queries.sql
@@ -0,0 +1,16 @@
+-- 1. What is the total amount each customer spent at the restaurant?
+select sales.customer_id, sum(menu.price) as total
+from sales join menu
+on sales.product_id = menu.product_id
+group by customer_id;
+
+
+-- 2. How many days has each customer visited the restaurant?
+select customer_id, count(distinct order_date) as count_visit from sales group by customer_id;
+
+
+-- 3. What was the first item from the menu purchased by each customer?
+select distinct on (customer_id) customer_id, product_id from sales order by customer_id, order_date;
+
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
