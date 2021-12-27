c805a7cc5a6fbc680cb71a1f8490204c59cd9835
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2021-12-27 17:26:13 +0100

    progress Case2 and correct markdown solutions

--- 2_Pizza_Runner/queries.sql
+++ 2_Pizza_Runner/queries.sql
@@ -6,5 +6,24 @@ select count(pizza_id) from customer_orders;
 select count(distinct order_id) from customer_orders;
 
 -- A3. How many successful orders were delivered by each runner?
+select runner_id, count(distinct order_id) from runner_orders where pickup_time != 'null' group by runner_id;
 
-a
+-- A4. How many of each type of pizza was delivered?
+select pizza_id, count(*) from customer_orders co
+join runner_orders ro
+on co.order_id = ro.order_id where pickup_time != 'null' group by pizza_id;
+
+-- A5. How many Vegetarian and Meatlovers were ordered by each customer?
+select pizza_name, count(*) from customer_orders co
+join pizza_names pn
+on co.pizza_id = pn.pizza_id group by pn.pizza_name;
+
+-- A6. What was the maximum number of pizzas delivered in a single order?
+with count_table as (
+    select co.order_id, count(pizza_id)
+    from customer_orders co join runner_orders ro on co.order_id = ro.order_id 
+    where pickup_time != 'null'
+    group by co.order_id order by co.order_id
+) select max(count) from count_table;
+
+-- A7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
